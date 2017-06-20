3f67c784d8c3a108223c8763326ed476fdedecdc
Eyvette Thompson <eyvettet@gmail.com>
2017-06-20 13:54:58 -0400

    Add files via upload

---
+++ ioVarsLab.py
@@ -0,0 +1,48 @@
+age = 48
+name = "Eyvette Thompson"
+gpa = 3.5
+
+print "I am {} years old".format(age)
+print "My name is {}".format(name)
+print "In school my GPA was {}".format(gpa)
+
+operand1 = 27.882
+operand2 = 258.03
+
+print "{} + {} = {}".format(operand1,operand2,operand1+operand2)
+print "{} - {} = {}".format(operand1,operand2,operand1-operand2)
+print "{} * {} = {}".format(operand1,operand2,operand1*operand2)
+print "{} / {} = {}".format(operand1,operand2,operand1/operand2)
+
+del operand1
+del operand2
+
+operand1 = 978.425
+operand2 = 0.0324
+
+print "{} + {} = {}".format(operand1,operand2,operand1+operand2)
+print "{} - {} = {}".format(operand1,operand2,operand1-operand2)
+print "{} * {} = {}".format(operand1,operand2,operand1*operand2)
+print "{} / {} = {}".format(operand1,operand2,operand1/operand2)
+
+print "{} % {} = {}".format(9,3,9 % 3)
+print "{} % {} = {}".format(112,10, 112 % 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
